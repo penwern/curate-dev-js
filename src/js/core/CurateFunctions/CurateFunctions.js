5dd3fbc25afb9bc7ef1a16ecad33e0cf25767b89
John Mackey <tekapeel@gmail.com>
2025-06-23 16:08:10 +0100

    added a new Curate helper to delegate global events efficiently

--- src/js/core/CurateFunctions/CurateFunctions.js
+++ src/js/core/CurateFunctions/CurateFunctions.js
@@ -1,21 +1,23 @@
-import CurateApi from './CurateApi.js';
-import CurateWorkspaces from './CurateWorkspaces.js';
-import CurateUi from './CurateUi.js';
-import CurateMetadata from './CurateMetadata.js';
-import CurateContextualHelp from './CurateContextualHelp.js';
-const Curate = (function() {
-    const api = CurateApi;
-    const workspaces = CurateWorkspaces;
-    const ui = CurateUi;
-    const metadata = CurateMetadata;
-    const contextualHelp = CurateContextualHelp;
-    return {
-        api,
-        workspaces,
-        ui,
-        metadata,
-        contextualHelp
-    };
+import CurateApi from "./CurateApi.js";
+import CurateWorkspaces from "./CurateWorkspaces.js";
+import CurateUi from "./CurateUi.js";
+import CurateMetadata from "./CurateMetadata.js";
+import CurateContextualHelp from "./CurateContextualHelp.js";
+import eventDelegator from "./CurateEvents.js";
+const Curate = (function () {
+  const api = CurateApi;
+  const workspaces = CurateWorkspaces;
+  const ui = CurateUi;
+  const metadata = CurateMetadata;
+  const contextualHelp = CurateContextualHelp;
+  return {
+    api,
+    workspaces,
+    ui,
+    metadata,
+    contextualHelp,
+    eventDelegator,
+  };
 })();
 // Export Curate so it's accessible globally
 window.Curate = Curate;
