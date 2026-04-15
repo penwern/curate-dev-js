df86315b5306cb6270c9aeaf8c4b61c2d6a60fa3
John Mackey <tekapeel@gmail.com>
2026-04-15 15:05:38 +0100

    remove old versioning logic from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const webpack = require("webpack");
-const fs = require("fs-extra");
 const packageJson = require("./package.json");
 const TerserPlugin = require("terser-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
@@ -19,15 +18,9 @@ module.exports = (env, argv) => {
       },
     },
     output: {
-      // In development, output to dist root for easier dev server serving
-      // In production, output to versioned directory
-      filename: isProduction
-        ? `[name]_${packageJson.version}.js`
-        : `[name].js`,
+      filename: "[name].js",
       chunkFilename: "[name].[chunkhash].js",
-      path: isProduction
-        ? path.resolve(__dirname, "dist", packageJson.version)
-        : path.resolve(__dirname, "dist"),
+      path: path.resolve(__dirname, "dist"),
       globalObject: "this",
       clean: true,
     },
@@ -36,84 +29,22 @@ module.exports = (env, argv) => {
         "process.env.VERSION": JSON.stringify(packageJson.version),
       }),
 
-      // Copy replaywebpage and worker library files to dist
       new CopyWebpackPlugin({
         patterns: [
           {
             from: path.resolve(__dirname, "node_modules/replaywebpage/sw.js"),
-            to: path.resolve(__dirname, isProduction ? `dist/${packageJson.version}` : "dist", "replaywebpage-sw.js"),
+            to: path.resolve(__dirname, "dist/replaywebpage-sw.js"),
           },
           {
             from: path.resolve(__dirname, "node_modules/replaywebpage/ui.js"),
-            to: path.resolve(__dirname, isProduction ? `dist/${packageJson.version}` : "dist", "replaywebpage-ui.js"),
+            to: path.resolve(__dirname, "dist/replaywebpage-ui.js"),
           },
           {
             from: path.resolve(__dirname, "node_modules/spark-md5/spark-md5.js"),
-            to: path.resolve(__dirname, isProduction ? `dist/${packageJson.version}` : "dist", "spark-md5.js"),
+            to: path.resolve(__dirname, "dist/spark-md5.js"),
           },
         ],
       }),
-
-      // Only run the custom plugin in production mode
-      ...(isProduction ? [
-        new CopyWebpackPlugin({
-          patterns: [
-            {
-              from: path.resolve(__dirname, "node_modules/replaywebpage/sw.js"),
-              to: path.resolve(__dirname, "dist/@latest/replaywebpage-sw.js"),
-            },
-            {
-              from: path.resolve(__dirname, "node_modules/replaywebpage/ui.js"),
-              to: path.resolve(__dirname, "dist/@latest/replaywebpage-ui.js"),
-            },
-            {
-              from: path.resolve(__dirname, "node_modules/spark-md5/spark-md5.js"),
-              to: path.resolve(__dirname, "dist/@latest/spark-md5.js"),
-            },
-          ],
-        }),
-        {
-          apply(compiler) {
-            compiler.hooks.afterEmit.tapAsync(
-              "CopyLatestBuild",
-              (_, callback) => {
-                const versionDir = path.resolve(
-                  __dirname,
-                  "dist",
-                  packageJson.version
-                );
-                const latestDir = path.resolve(__dirname, "dist", "@latest");
-
-                // Make sure the @latest folder exists
-                fs.emptyDirSync(latestDir);
-
-                fs.readdir(versionDir, (err, files) => {
-                  if (err) {
-                    console.error("Error reading versioned files:", err);
-                    return callback();
-                  }
-
-                  const copyPromises = files.map(file => {
-                    const sourcePath = path.resolve(versionDir, file);
-                    const targetPath = path.resolve(
-                      latestDir,
-                      file.replace(`_${packageJson.version}.js`, ".js")
-                    );
-
-                    return fs.copy(sourcePath, targetPath).then(() => {
-                      console.log(`Successfully copied ${file} to @latest`);
-                    }).catch(copyErr => {
-                      console.error(`Error copying file to @latest: ${file}`, copyErr);
-                    });
-                  });
-
-                  Promise.all(copyPromises).then(() => callback()).catch(() => callback());
-                });
-              }
-            );
-          },
-        }
-      ] : []),
     ],
     module: {
       rules: [
