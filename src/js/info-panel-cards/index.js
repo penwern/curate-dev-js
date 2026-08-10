4f8fb16a8e6c2cb22bcd4c60ba08d2b601ef49f1
John Mackey <tekapeel@gmail.com>
2026-08-10 16:40:14 +0300

    new infopanel card for submission information

--- src/js/info-panel-cards/index.js
+++ src/js/info-panel-cards/index.js
@@ -1,5 +1,6 @@
 //import './FileInfoCard.jsx';
 import "./IngestionInfoCard.jsx";
+import "./SubmissionInfoCard.jsx";
 import "./PremisEventsCard.jsx";
 //import './PreservationDerivativesCard.jsx';
 import "./WordMetaCard.jsx";
